cd1b243d1af0455a91d9f57a9a7102608160a003
erik <eriktyryshkin@gmail.com>
2022-01-06 23:02:54 -0800

    added support for mtls, added codegen script for proto files, and added general config

--- src/server.py
+++ src/server.py
@@ -1,9 +1,9 @@
 import asyncio
-import os
 
-from dotenv import load_dotenv
 import grpc
 
+from config import HOST_PORT, HOST_ADDRESS, MTLS_ENABLED, SECRETS_DIR, \
+    SERVER_CERT_PATH, SERVER_KEY_PATH, CLIENT_CERT_PATH
 from db.account_store import AccountStore
 from api.gvoice_handler import GVoiceHandler
 import api.gvoice_pb2
@@ -11,22 +11,28 @@ import api.gvoice_pb2_grpc
 
 
 async def serve() -> None:
+    listen_addr = f'{HOST_ADDRESS}:{HOST_PORT}'
     server = grpc.aio.server()
+
     api.gvoice_pb2_grpc.add_GVoiceServicer_to_server(GVoiceHandler(), server)
-    listen_addr = '[::]:50051'
-    server.add_insecure_port(listen_addr)
+
+    if MTLS_ENABLED:
+        server_credentials = grpc.ssl_server_credentials(
+            [[open(SERVER_KEY_PATH, 'rb').read(), open(SERVER_CERT_PATH, 'rb').read()],
+             ],
+            root_certificates=open(CLIENT_CERT_PATH, 'rb').read(),
+            require_client_auth=True
+        )
+        server.add_secure_port(listen_addr, server_credentials)
+    else:
+        server.add_insecure_port(listen_addr)
+        
     await server.start()
     await server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    load_dotenv()
-
-    secrets_dir = os.environ.get('SECRETS_DIR', None)
-    if not secrets_dir:
-        raise ValueError(
-            'Make sure .env file exists and includes all env vars')
-    db = AccountStore.get_instance(secrets_dir)
+    db = AccountStore.get_instance(SECRETS_DIR)
     db.load_accounts()
 
     asyncio.run(serve())
